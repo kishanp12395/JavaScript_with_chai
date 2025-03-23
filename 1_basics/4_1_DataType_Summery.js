032a2a78b17cb2e30e5af0154c8d76b1436015d1
Kishan Kumar Prajapati <kkp12395@gmail.com>
2025-03-23 05:30:12 +0000

    stack or heap

--- 1_basics/4_1_DataType_Summery.js
+++ 1_basics/4_1_DataType_Summery.js
@@ -35,3 +35,40 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***********************************************
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubeName = 'Kishan p';
+let anotherName = myYoutubeName;
+anotherName = 'Chai Code';
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = 'Kishan@google.com';
+console.log(userOne.email);
+console.log(userTwo.email);
