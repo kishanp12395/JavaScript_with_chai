94c7ceb3b972df4349a49c2392fa107d9bc4b449
Kishan Kumar Prajapati <kkp12395@gmail.com>
2025-03-26 13:02:01 +0000

    object destructure

---
+++ 2_basics/5_object.js
@@ -0,0 +1,12 @@
+// destructuring of object
+
+const course = {
+    coursename: "Js in Hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course. courseInstructor
+
+const {courseInstructor} = course
+console.log(courseInstructor);
