fe48c706f7b5daee637ef39c27782463c2ca4e8b
Kishan Kumar Prajapati <kkp12395@gmail.com>
2025-03-31 03:39:22 +0000

    switch

---
+++ 4_control_flow/two.js
@@ -0,0 +1,14 @@
+
+let month = 3
+switch (month) {
+    case 5:
+        console.log("may");
+        break;
+    case 3:
+        console.log("march");
+        // break;
+
+    default:
+        console.log("Invalid month");
+        break;
+}
